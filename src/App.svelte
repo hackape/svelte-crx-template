303d2060dce592c7e7058ebe386f79330d899a55
hackape <v@hackape.com>
2024-04-03 12:32:24 +0800

    init

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+</script>
+
+<main>
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer"
+      >SvelteKit</a
+    >, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+</main>
+
+<style>
+</style>
